ef9dd0ebb54e22d399b3ae1da5ff7e2f70c29447
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-01-30 17:52:53 -0300

    adding routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
