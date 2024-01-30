40de271aa6bcb240804b167ecd2747a1d414f4c7
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-01-30 17:31:50 -0300

    changes home-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { HeaderComponent } from './header/header.component';
-=======
->>>>>>> c4c3f55ce62c6a2830fd107cb5f2e454d302b976
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent],
-=======
-  imports: [CommonModule, RouterOutlet, HomeComponent],
->>>>>>> c4c3f55ce62c6a2830fd107cb5f2e454d302b976
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
