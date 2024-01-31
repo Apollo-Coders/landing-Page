20acd1430c677f4812f23d2133c71b05ddb2de5f
Daiane Bolzan <dainha85@gmail.com>
2024-01-31 17:08:45 -0300

    feat: add routes to contact and portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: 'home' },
 ];
