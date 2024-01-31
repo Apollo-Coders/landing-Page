c4507d79b72de5d46d743a13da6982ca21795073
Guilherme Duarte <guilhduart.abr@gmail.com>
2024-01-31 12:00:28 -0300

    rota de redirecionamento para home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'aboutus', component: AboutUsComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutUsComponent },
 ];
