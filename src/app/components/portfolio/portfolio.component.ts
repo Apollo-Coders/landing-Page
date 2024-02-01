6bef2836ab2047f53ec326cfd8a13c3571bbe8e1
Daiane Bolzan <dainha85@gmail.com>
2024-02-01 15:42:04 -0300

    feat: add content to page

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ParagraphComponent } from '../about-us/paragraph/paragraph.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ParagraphComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
